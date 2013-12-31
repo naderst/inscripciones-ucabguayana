fbcdc151ded08e7ecad0102fe0ad72309a7063e9
josecols <josecolsg@gmail.com>
2013-12-31 13:51:19 -0430

    Agregado nuevamente config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+$app['basedir'] = '/inscripciones-ucabguayana';
+$app['db'] = array(
+	'host' => 'localhost',
+	'name' => 'inscripcion',
+	'user' => 'postgres',
+	'pass' => '',
+	'port' => '5432'
+);
+?>
