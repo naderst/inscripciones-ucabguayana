2f4a3f69962101355828a304e3779bc82211ae55
josecols <josecolsg@gmail.com>
2014-01-17 11:57:04 -0430

    Agregado mensaje en caso de que el estudiante esté en hold

--- json/login.php
+++ json/login.php
@@ -6,27 +6,38 @@ $conexion = pg_connect("host=".$app["db"]["host"]." port=".$app["db"]["port"]." 
 $mensaje = array();
 $login = pg_query("select id_alumno
                     from alumnos
-                    where id_alumno = '$_POST[usuario]' and
+                    where id_alumno = $_POST[usuario] and
                           clave = md5('$_POST[clave]')");
 if(($tupla = pg_fetch_array($login))){
-    session_start();
-    $_SESSION['usuario'] = $tupla['id_alumno'];
-    $tupla = pg_fetch_assoc(pg_query("select nombre_alumno,apellido_alumno
-                                       from alumnos
-                                       where id_alumno = $tupla[id_alumno]"));
-    $mensaje['flag'] = 1;
-    $mensaje['nombre'] = $tupla['nombre_alumno']." ".$tupla['apellido_alumno'];
-    $tupla = pg_fetch_assoc(pg_query("select count(*) as inscritas
-                                       from materias_x_alumnos inner join (select max(lapso) as lapso
-                                                                            from lapsos) as periodo
-                                            on materias_x_alumnos.lapso = periodo.lapso 
-                                            and materias_x_alumnos.id_alumno = $_SESSION[usuario]"));
-   $mensaje['msg'] = ($tupla['inscritas']==0)?"/prematricula/":"/prematricula-fija/";
+    $hold = pg_query("select nombre_hold,descripcion_hold 
+                        from alumnos_x_holds inner join holds
+                            on(alumnos_x_holds.id_hold = holds.id_hold
+                             and alumnos_x_holds.id_alumno = $_POST[usuario])");
+    if(pg_num_rows($hold)!=0){
+        $mensaje['flag'] = 0;
+        $mensaje['msg'] = "";
+        while (($tupla=  pg_fetch_assoc($hold)))
+                $mensaje['msg'] .= "$tupla[nombre_hold] : $tupla[descripcion_hold] | ";
+    }else{
+        session_start();
+        $_SESSION['usuario'] = $tupla['id_alumno'];
+        $tupla = pg_fetch_assoc(pg_query("select nombre_alumno,apellido_alumno
+                                           from alumnos
+                                           where id_alumno = $tupla[id_alumno]"));
+        $mensaje['flag'] = 1;
+        $mensaje['nombre'] = $tupla['nombre_alumno']." ".$tupla['apellido_alumno'];
+        $tupla = pg_fetch_assoc(pg_query("select count(*) as inscritas
+                                           from materias_x_alumnos inner join (select max(lapso) as lapso
+                                                                                from lapsos) as periodo
+                                                on materias_x_alumnos.lapso = periodo.lapso 
+                                                and materias_x_alumnos.id_alumno = $_SESSION[usuario]"));
+       $mensaje['msg'] = ($tupla['inscritas']==0)?"/prematricula/":"/prematricula-fija/";
+    }
 }
 else{
     $login = pg_query("select id_profesor
                         from cuentas_x_profesores
-                        where id_profesor = '$_POST[usuario]' and
+                        where id_profesor = $_POST[usuario] and
                               clave = md5('$_POST[clave]')");
     if(($tupla = pg_fetch_array($login))){
         session_start();
